7812467db86274bd75cdc4e99fbfa17066d4aa4f
allcui <xend@live.ca>
2021-04-25 21:54:08 -0400

    added some sliders to control durations

--- lib/screen/interval_timer.dart
+++ lib/screen/interval_timer.dart
@@ -1,47 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 class IntervalTimer extends StatefulWidget {
-  static const title = 'Interval Timer';
+  static const String title = 'Interval Timer';
+  static const int maxWorkDurationInSeconds = 180;
+  static const int maxRestDurationInSeconds = 120;
+  static const int maxWarmUpDurationInSeconds = 60;
+  static const int maxSets = 20;
   @override
   _IntervalTimerState createState() => _IntervalTimerState();
 }
 
-class _IntervalTimerState extends State<IntervalTimer> {
+class _IntervalTimerState extends State<IntervalTimer> with TickerProviderStateMixin{
 
-  final CountDownController _countDownController = CountDownController();
+  AnimationController _countdownController;
+  Animation _countdownAnimation;
 
-  int _remainingDuration = 30;
+  int _remainingWorkDuration = 60;
+  int _remainingRestDuration = 30;
+  int _remainingWarmUpDuration = 30;
+  int _remainingSets = 10;
 
   @override
   Widget build(BuildContext context) {
-    final Widget timer = CircularCountDownTimer(
-      duration: _remainingDuration,
-      initialDuration: 0,
-      controller: _countDownController,
-      width: 200.0,
-      height: 300.0,
-      ringColor: Colors.orangeAccent,
-      ringGradient: null,
-      fillColor: Colors.purpleAccent[100],
-      fillGradient: null,
-      backgroundColor: Colors.purple[500],
-      backgroundGradient: null,
-      strokeWidth: 10.0,
-      strokeCap: StrokeCap.round,
-      textStyle: TextStyle(
-          fontSize: 50.0, color: Colors.white, fontWeight: FontWeight.bold),
-      textFormat: CountdownTextFormat.S,
-      isReverse: true,
-      isReverseAnimation: false,
-      isTimerTextShown: false,
-      autoStart: false,
-      onStart: () {
-        print('Countdown Started');
-      },
-      onComplete: () {
-        print('Countdown Ended');
-      },
+    final Widget timer = Text(
+      durationToString(_remainingWorkDuration),
+      style: TextStyle(
+        fontSize: 150.0,
+      ),
+    );
+    final Widget warmUpDurationSlider = Row(
+      children: <Widget>[
+        Text('Warm Up Duration(MM:SS): '),
+        Slider(
+          divisions: IntervalTimer.maxWarmUpDurationInSeconds ~/ 15,
+          value: _remainingWarmUpDuration.toDouble(),
+          min: 0,
+          max: IntervalTimer.maxWarmUpDurationInSeconds.toDouble(),
+          onChanged: (newDuration) {
+            setState(() {_remainingWarmUpDuration = newDuration.toInt();}
+            );
+          },
+        ),
+        Text(durationToString(_remainingWarmUpDuration)),
+      ],
+    );
+    final Widget workDurationSlider = Row(
+      children: <Widget>[
+        Text('Work Duration(MM:SS): '),
+        Slider(
+          divisions: IntervalTimer.maxWorkDurationInSeconds ~/ 15,
+          value: _remainingWorkDuration.toDouble(),
+          min: 0,
+          max: IntervalTimer.maxWorkDurationInSeconds.toDouble(),
+          onChanged: (newDuration) {
+            setState(() {_remainingWorkDuration = newDuration.toInt();}
+          );
+          },
+        ),
+        Text(durationToString(_remainingWorkDuration)),
+      ],
+    );
+    final Widget restDurationSlider = Row(
+      children: <Widget>[
+        Text('Rest Duration(MM:SS): '),
+        Slider(
+          divisions: IntervalTimer.maxRestDurationInSeconds ~/ 15,
+          value: _remainingRestDuration.toDouble(),
+          min: 0,
+          max: IntervalTimer.maxRestDurationInSeconds.toDouble(),
+          onChanged: (newDuration) {
+            setState(() {_remainingRestDuration = newDuration.toInt();}
+            );
+          },
+        ),
+        Text(durationToString(_remainingRestDuration)),
+      ],
+    );
+    final Widget setCountSlider = Row(
+      children: <Widget>[
+        Text('Set of Intervals: '),
+        Slider(
+          value: _remainingSets.toDouble(),
+          min: 0,
+          max: IntervalTimer.maxSets.toDouble(),
+          onChanged: (newSetCount) {
+            setState(() {_remainingSets = newSetCount.toInt();}
+            );
+          },
+        ),
+        Text(_remainingSets.toString()),
+      ],
     );
     return Scaffold(
       appBar: AppBar(title: Text(IntervalTimer.title),),
@@ -51,11 +99,25 @@ class _IntervalTimerState extends State<IntervalTimer> {
           child: Column(
             children: <Widget>[
               timer,
-              IconButton(icon: Icon(Icons.play_arrow, color: Colors.white,), onPressed: () => _countDownController.start()),
+              warmUpDurationSlider,
+              workDurationSlider,
+              restDurationSlider,
+              setCountSlider
             ],
           ),
         ),
       ),
     );
   }
+
+  String durationToString(int durationInSeconds){
+    final duration = Duration(seconds: durationInSeconds);
+    final minutes = duration.inMinutes;
+    final seconds = durationInSeconds % 60;
+
+    final minutesString = '$minutes'.padLeft(2, '0');
+    final secondsString = '$seconds'.padLeft(2, '0');
+    return '$minutesString:$secondsString';
+  }
 }
+
